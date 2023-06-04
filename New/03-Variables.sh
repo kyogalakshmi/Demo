94ce295c86152811f624b358563408b25b4cb717
Yoga <kyogalakshmi18@gmail.com>
2023-06-04 19:49:36 +0530

    fefe

--- New/03-Variables.sh
+++ New/03-Variables.sh
@@ -4,5 +4,5 @@ c=12;
 
 #echo $a // print the value of the variable
 echo Value of the first variable is $a
-echo Value of the second variable is $b
+echo Value of the second variable is ${b}
 echo Value of the third Variable is $c
