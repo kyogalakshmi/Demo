d2765ad63203ee0ca713036f61c09396d63499bd
Yoga <kyogalakshmi18@gmail.com>
2023-06-04 21:14:04 +0530

    fefe

---
+++ New/05-specialcharacters.sh
@@ -0,0 +1,8 @@
+#echo $0 #prints the script name yu re executing
+
+# till $0 to $9
+
+echo "Name of the script is $0"
+echo  "Name of the trainer is $1"
+echo "Training Batch Number is $3"
+echo "Current Topics is $3"
