fa29e02887ef1a141652e7a7b1100cc39276d3a8
Yoga <kyogalakshmi18@gmail.com>
2023-06-04 21:01:52 +0530

    fefe

--- New/03-Variables.sh
+++ New/03-Variables.sh
@@ -4,5 +4,7 @@ c=12;
 
 #echo $a // print the value of the variable
 echo Value of the first variable is $a
-echo Value of the second variable is ${b}
+echo Value of the second variable is ${b} // new syntax
 echo Value of the third Variable is $c
+
+#Dynamic variables
