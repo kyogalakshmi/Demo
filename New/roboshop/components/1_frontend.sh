f4ea3b8bb531aaf64d54185bd57729ba5eefb7a2
Yoga <kyogalakshmi18@gmail.com>
2023-06-06 20:55:15 +0530

    fefe

--- New/roboshop/components/1_frontend.sh
+++ New/roboshop/components/1_frontend.sh
@@ -12,13 +12,12 @@
 # mv static/* .
 # rm -rf frontend-main README.md
 # mv localhost.conf /etc/nginx/default.d/roboshop.conf
-COMPONENT =frontend
+COMPONENT=frontend
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then
   echo -e "\e[31m This is expected to run by a root user with a sudo privelege\e[0m"
   exit 1 
 fi
-
 echo "Installing Nginx :"
 yum install nginx -y &>> "/tmp/$COMPONENT.log"
 
