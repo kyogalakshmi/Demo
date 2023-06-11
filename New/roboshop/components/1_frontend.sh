794090e8b63973815c40fc4caa567871fc047439
Yoga <kyogalakshmi18@gmail.com>
2023-06-11 17:54:28 +0530

    fefe

--- New/roboshop/components/1_frontend.sh
+++ New/roboshop/components/1_frontend.sh
@@ -18,12 +18,27 @@ if [ $ID -ne 0 ] ; then
   echo -e "\e[31m This is expected to run by a root user with a sudo privelege\e[0m"
   exit 1 
 fi
-echo "Installing Nginx :"
-yum install nginx -y &>> "/tmp/$COMPONENT.log"
+stat(){
 if [ $1 -eq 0 ] ; then
   echo -e "\e[32m success \e[0m"
 else
   echo -e "\e[31m failure \e[0m"
   fi
+}
+echo "Installing Nginx :"
+yum install nginx -y &>> "/tmp/$COMPONENT.log"
+stat $?
   echo -n "Downloading the $COMPONENT component :"
-  curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+  curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $?
+echo -n "Performing Cleanup:"
+cd /usr/share/nginx/html
+rm -rf * &>> "/tmp/$COMPONENT.log"
+stat $?
+
+echo -n "Extracting $COMPONENT component"
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
