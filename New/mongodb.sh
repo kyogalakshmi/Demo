d6e083a1ab9843bb7b35b98920d2091eb86b3477
Yoga <kyogalakshmi18@gmail.com>
2023-06-12 20:55:02 +0530

    fefe

---
+++ New/mongodb.sh
@@ -0,0 +1,18 @@
+COMPONENT=mongodb
+LOGFILE = "/tmp/${COMPONENT}.log"
+ID=$(id -u)
+if [ $ID -ne 0 ] ; then
+  echo -e "\e[31m This is expected to run by a root user with a sudo privelege\e[0m"
+  exit 1 
+fi
+stat(){
+if [ $1 -eq 0 ] ; then
+  echo -e "\e[32m success \e[0m"
+else
+  echo -e "\e[31m failure \e[0m"
+  fi
+}
+
+echo -n "Configuring the $COMPONENT repo : "
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+stat $?
