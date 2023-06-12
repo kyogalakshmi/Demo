7442cdcfc6982ce80f50240068f7b0da31d07f4f
Yoga <kyogalakshmi18@gmail.com>
2023-06-12 21:10:21 +0530

    fefe

--- New/mongodb.sh
+++ New/mongodb.sh
@@ -16,3 +16,12 @@ else
 echo -n "Configuring the $COMPONENT repo : "
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
 stat $?
+
+echo -n "Installing $COMPONENT : "
+yum install -y $COMPONENT-org &>> $LOGFILE
+stat $?
+
+echo -n "Starting $COMPONENT : "
+systemctl enable mongod &>> $LOGFILE
+systemctl start mongod &>> $LOGFILE
+stat $?
