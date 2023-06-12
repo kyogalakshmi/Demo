f97b59ebdbb9e59918a29ae957a671e1c384e5c4
Yoga <kyogalakshmi18@gmail.com>
2023-06-12 22:30:08 +0530

    fefe

--- New/mongodb.sh
+++ New/mongodb.sh
@@ -27,7 +27,7 @@ sudo systemctl start mongod       &>> "/tmp/$COMPONENT.log"
 stat $?
 
 echo -n "Enabling the DB Visisbility : "
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongo.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 stat $?
 
 echo -n "Starting $COMPONENT : "
